0e75c6d859a349d9a0f85bed740387b26aa46ad1
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2016-02-04 16:05:38 -0600

    Another try for sauceLabs

--- karma.config.js
+++ karma.config.js
@@ -11,29 +11,17 @@ const reporters = [
 ];
 var browsers = ['PhantomJS']; // for local builds
 var sauceLunchers = {
-    sl_chrome: {
+    SL_Chrome: {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Windows 7',
       version: '35'
     },
-    sl_firefox: {
+    SL_FireFox: {
       base: 'SauceLabs',
       browserName: 'firefox',
       version: '30'
     },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
   }
 
 if (process.env.TRAVIS) {
